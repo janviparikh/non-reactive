3babdb92a0aa2b24ca803c001363e261c311fd07
Janvi Parikh <parikh.janvi94@gmail.com>
2019-09-24 10:11:02 +0530

    Fetch the valueAsync using CompletableFuture

--- src/main/kotlin/com/thoughtworks/nonreactive/ApplicationController.kt
+++ src/main/kotlin/com/thoughtworks/nonreactive/ApplicationController.kt
@@ -2,15 +2,27 @@ package com.thoughtworks.nonreactive
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.concurrent.CompletableFuture
+import org.springframework.web.context.request.async.DeferredResult
+import java.util.concurrent.Executors
+import java.util.function.Supplier
 
 
 @RestController
 class ApplicationController {
 
+    private val threadPool = Executors.newCachedThreadPool()
+
     @GetMapping("welcome-delay")
-    fun greetWithDelay(): WelcomeMessage {
+    fun greetWithDelay(): DeferredResult<WelcomeMessage> {
+        val deferredResult = DeferredResult<WelcomeMessage>()
+        CompletableFuture
+                .supplyAsync(Supplier<Any> {
                     Thread.sleep(1000)
-        return WelcomeMessage("Hello")
+                    WelcomeMessage("Hello")
+                }, threadPool)
+                .whenCompleteAsync{ result, throwable -> deferredResult.setResult(result as WelcomeMessage) }
+        return deferredResult
     }
 }
 
